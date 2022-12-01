7a7353ad0c2ec808b6ace9d7e12b60a2c428006e
bobbyzhong <3bobbyzhong3@gmail.com>
2022-12-01 02:56:34 -0800

    played with typewriter effect and fixed styling

---
+++ components/TypewriterC.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import styles from "../styles/Home.module.css";
+
+export default function TypewriterC() {
+    return (
+        <div className={styles.typewriter}>
+            <div className={styles.typewriter1}>Simple web solutions for</div>
+            <div className={styles.typewriter2}>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("restaurants")
+                            .pauseFor(1500)
+                            .deleteAll()
+                            .typeString("gyms")
+                            .pauseFor(1500)
+                            .deleteAll()
+                            .typeString("aerospace companies")
+                            .pauseFor(1500)
+                            .deleteAll()
+                            .typeString("law firms")
+                            .start();
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
