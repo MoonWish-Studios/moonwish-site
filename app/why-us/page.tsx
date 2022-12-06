5f58625a554d6e48bfdd06eae730b30212ad6ebe
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-06 00:06:44 -0800

    added css variables to use

---
+++ app/why-us/page.tsx
@@ -0,0 +1,8 @@
+export default function WhyUs() {
+  return (
+    <div>
+      <p className="test">hi my name is weibo zhang</p>
+      <p className="test2">hi my name is weibo zhang</p>
+    </div>
+  );
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,55 +1,49 @@
+// "use client";
 import Image from "next/image";
 import React from "react";
 
 export default function Navbar() {
-    return (
-        <div className="nav">
-            <div>
-                <a href="/" className="logo">
-                    <img
-                        src={"/assets/logo.svg"}
-                        style={{ width: 100, height: "auto" }}
-                        alt=""
-                        className="logoImg"
-                    />
-                </a>
-            </div>
+  return (
+    <div className="nav">
+      <div>
+        <a href="/" className="logo">
+          <img
+            src={"/assets/logo.svg"}
+            style={{ width: 100, height: "auto" }}
+            alt=""
+            className="logoImg"
+          />
+        </a>
+      </div>
 
-            <div className="nav-links" id="navLinks">
-                <ul>
-                    <li>
-                        <a href="">Why Us</a>
-                    </li>
-                    <CustomLink href="/services">Services</CustomLink>
-                    <li>
-                        <a href="">FAQ</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="contact">
-                <ul>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+      <div className="nav-links" id="navLinks">
+        <ul>
+          <li>
+            <a href="">Why Us</a>
+          </li>
+          <CustomLink href="/services">Services</CustomLink>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="contact">
+        <ul>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-function CustomLink({
-    href,
-    children,
-    ...props
-}: {
-    href: string;
-    children: string;
-}) {
-    const path = window.location.pathname;
+function CustomLink({ href, children, ...props }: { href: string; children: string }) {
+  const path = window.location.pathname;
 
-    return (
-        <li className={path === href ? "active" : ""}>
-            <a href={href}>{children}</a>
-        </li>
-    );
+  return (
+    <li className={path === href ? "active" : ""}>
+      <a href={href}>{children}</a>
+    </li>
+  );
 }
