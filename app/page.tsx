89be9c82fa56c069bb6876f792d349a771da7a00
bobbyzhong <3bobbyzhong3@gmail.com>
2022-12-05 18:37:14 -0800

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -27,14 +27,4 @@ export default function Home() {
             </main>
         </div>
     );
-    return (
-        <div className={`${josefin_sans.className} container`}>
-            <Navbar />
-            <main className="main">
-                <TypewriterC />
-                <Button></Button>
-                empty space more empty space
-            </main>
-        </div>
-    );
 }
