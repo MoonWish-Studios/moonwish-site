6e1d1a4cf50054f8039311ca8c6cc01ecd59752f
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-06 23:47:31 -0800

    fix footer

--- app/[404]/page.tsx
+++ app/[404]/page.tsx
@@ -1,17 +1,24 @@
 "use client";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
+import astro404 from "../../public/animations/404-lottie.json";
+import lottie from "lottie-web";
+
 export default function Custom404() {
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: document.querySelector(".lottie-player")!,
+      path: "/animations/data.json",
+      renderer: "svg",
+    });
+
+    return () => lottie.destroy();
+  }, []);
   return (
     <div className="fill-screen">
       <div>
-        <Player
-          autoplay
-          loop
-          src="https://assets10.lottiefiles.com/packages/lf20_2QSlz3Li88.json"
-          className="lottie-404"
-        />
-        {/* <Lottie animationData={astronaut404Animation} loop={true} className="lottie-404" /> */}
+        <div className="lottie-player" />
+
         <div>We were unable to find the page you were looking for :(</div>
       </div>
     </div>
