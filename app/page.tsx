b7f626861ff3971ea063a8a6f8437fde174aac06
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-10 15:17:02 -0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
       <main className="main">
         {/* <TypewriterC /> */}
         {/* <Button></Button> */}
+        <button>asfasf</button>
       </main>
     </div>
   );
