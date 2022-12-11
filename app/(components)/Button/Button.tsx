c4c1e9fbf981b403995d94d98bb684a7fd6d51df
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-11 01:56:16 -0800

    remove buttonstyle import

--- app/(components)/Button/Button.tsx
+++ app/(components)/Button/Button.tsx
@@ -1,21 +1,20 @@
 "use client";
-import StyledButton from "./ButtonStyle";
 
 import React from "react";
 import Link from "next/link";
 function clickMe() {
-    console.log("Clicked");
+  console.log("Clicked");
 }
 
 export default function Button() {
-    return (
-        <>
-            {/* <button onClick={clickMe}> */}
-            {/* <p>Learn More</p> */}
-            <Link className="next-link button" href="#">
-                Learn More
-            </Link>
-            {/* </button> */}
-        </>
-    );
+  return (
+    <>
+      {/* <button onClick={clickMe}> */}
+      {/* <p>Learn More</p> */}
+      <Link className="next-link button" href="#">
+        Learn More
+      </Link>
+      {/* </button> */}
+    </>
+  );
 }
