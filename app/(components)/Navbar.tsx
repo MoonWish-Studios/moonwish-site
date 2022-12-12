5b0f73fffe734c377ca1d5608c4bd3d16ce06dc5
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-11 17:17:24 -0800

    updated navbar

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -1,53 +1,50 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 export default function Navbar() {
-    return (
-        <div className="nav">
-            <div>
-                <a href="/" className="logo">
-                    <img
-                        src={"/assets/logo.svg"}
-                        style={{ width: 100, height: "auto" }}
-                        alt=""
-                        className="logoImg"
-                    />
-                </a>
-            </div>
+  return (
+    <div className="nav">
+      <div>
+        <Link href="/" className="logo">
+          <Image
+            src={"/assets/logo.svg"}
+            style={{ width: 100, height: "auto" }}
+            alt=""
+            height={60}
+            width={100}
+            className="logoImg"
+          />
+        </Link>
+      </div>
 
-            <div className="nav-links" id="navLinks">
-                <ul>
-                    <CustomLink href="/">Why Us</CustomLink>
-                    <CustomLink href="/services">Services</CustomLink>
-                    <CustomLink href="/faq">FAQ</CustomLink>
-                </ul>
-            </div>
-            <div className="contact">
-                <ul>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+      <div className="nav-links" id="navLinks">
+        <ul>
+          <CustomLink href="/why-us">Why Us</CustomLink>
+          <CustomLink href="/services">Services</CustomLink>
+          <CustomLink href="/faq">FAQ</CustomLink>
+        </ul>
+      </div>
+      <Link className="next-link" href="/contact">
+        Contact
+      </Link>
+      {/* <ul>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul> */}
+    </div>
+  );
 }
 
-function CustomLink({
-    href,
-    children,
-    ...props
-}: {
-    href: string;
-    children: string;
-}) {
-    // const path = window.location.pathname;
+function CustomLink({ href, children }: { href: string; children: string }) {
+  // const path = window.location.pathname;
 
-    return (
-        <li className={href ? "active" : ""}>
-            <a href={href}>{children}</a>
-        </li>
-    );
+  return (
+    <li>
+      <Link className="next-link" href={href}>
+        {children}
+      </Link>
+    </li>
+  );
 }
