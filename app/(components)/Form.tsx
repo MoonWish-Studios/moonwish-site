12043aa5dfc204330bd0d3484753f8cf0362349c
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-13 15:57:30 -0800

    separated form to own component

---
+++ app/(components)/Form.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Textbox from "./Textbox";
+
+export default function Form({ state, handleSubmit }: { state: any; handleSubmit: any }) {
+  return (
+    <form onSubmit={handleSubmit} className="contact-details">
+      <Textbox state={state} regular label="Your Name" name="name" />
+      <Textbox state={state} regular label="Phone Number" name="number" type="tel" />
+      <Textbox state={state} regular label="Business Name" name="businessName" />
+      <Textbox state={state} regular label="Business Email" name="businessEmail" type="email" />
+      <Textbox state={state} regular label="Location" name="location" />
+      <Textbox state={state} regular label="Website (leave blank if none)" name="website" />
+      <Textbox state={state} large label="How Can We Help You?" name="message" />
+      <button type="submit" disabled={state.submitting} className="colorful-button grid-span-col">
+        Submit
+      </button>
+    </form>
+  );
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import Textbox from "../(components)/Textbox";
 import LottieAstronaut from "../(components)/LottieAstronaut";
 import { useForm } from "@formspree/react";
+import Form from "../(components)/Form";
 export default function Contact() {
   return (
     <div className="contact-section">
@@ -37,22 +38,7 @@ function ContactDetails() {
   );
 }
 
-function Form({ state, handleSubmit }: { state: any; handleSubmit: any }) {
-  return (
-    <form onSubmit={handleSubmit} className="contact-details">
-      <Textbox state={state} regular label="Your Name" name="name" />
-      <Textbox state={state} regular label="Phone Number" name="number" type="tel" />
-      <Textbox state={state} regular label="Business Name" name="businessName" />
-      <Textbox state={state} regular label="Business Email" name="businessEmail" type="email" />
-      <Textbox state={state} regular label="Location" name="location" />
-      <Textbox state={state} regular label="Website (leave blank if none)" name="website" />
-      <Textbox state={state} large label="How Can We Help You?" name="message" />
-      <button type="submit" disabled={state.submitting} className="colorful-button grid-span-col">
-        Submit
-      </button>
-    </form>
-  );
-}
+Form;
 function BigHeader({
   heading,
   caption,
